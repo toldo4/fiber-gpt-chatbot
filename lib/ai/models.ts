8e13bf0a73bfac3cb92c816a9abd1a11ffe7f018
mansour <mansour@toldo.me>
2026-06-14 12:16:09 -0400

    disabled older models

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -10,27 +10,27 @@ export interface Model {
 }
 
 export const models: Array<Model> = [
-  {
-    id: 'gpt-4.1',
-    label: 'GPT 4.1',
-    apiIdentifier: 'gpt-4.1',
-    description: '',
-    provider: 'openai',
-  },
-    {
-    id: 'gpt-4o-mini',
-    label: 'GPT 4o mini',
-    apiIdentifier: 'gpt-4o-mini',
-    description: 'Small model for fast, lightweight tasks',
-    provider: 'openai',
-  },
-  {
-    id: 'gpt-4o',
-    label: 'GPT 4o',
-    apiIdentifier: 'gpt-4o',
-    description: 'For complex, multi-step tasks',
-    provider: 'openai',
-  },
+  // {
+  //   id: 'gpt-4.1',
+  //   label: 'GPT 4.1',
+  //   apiIdentifier: 'gpt-4.1',
+  //   description: '',
+  //   provider: 'openai',
+  // },
+  //   {
+  //   id: 'gpt-4o-mini',
+  //   label: 'GPT 4o mini',
+  //   apiIdentifier: 'gpt-4o-mini',
+  //   description: 'Small model for fast, lightweight tasks',
+  //   provider: 'openai',
+  // },
+  // {
+  //   id: 'gpt-4o',
+  //   label: 'GPT 4o',
+  //   apiIdentifier: 'gpt-4o',
+  //   description: 'For complex, multi-step tasks',
+  //   provider: 'openai',
+  // },
 // {
 //     id: 'gemini-2.5-pro',
 //     label: 'Gemini 2.5 Pro',
